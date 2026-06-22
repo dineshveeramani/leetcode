afb365540b8e5d3bbb7e4954cece16d908ac1521
dineshveeramani <dineshveeramani2006@gmail.com>
2026-06-22 11:49:09 +0530

    Create StepsToZero.java

---
+++ StepsToZero.java
@@ -0,0 +1,22 @@
+public class StepsToZero {
+
+    public static int numberOfSteps(int num) {
+        int steps = 0;
+
+        while (num > 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num -= 1;
+            }
+            steps++;
+        }
+
+        return steps;
+    }
+
+    public static void main(String[] args) {
+        int num = 14;
+        System.out.println(numberOfSteps(num));
+    }
+}
